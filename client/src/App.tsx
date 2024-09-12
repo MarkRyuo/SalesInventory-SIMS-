932bf4244466f7ae062857748ec083a4e599bb0c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-12 08:22:24 +0800

    Subject: Body

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,25 +12,24 @@ function App() {
               {/* Navbar */}
           </div>
           
-          <div className="container-lg">
+          <form className="container-lg">
               <div className="row">
-              <div className="col">
-                <div className="my-2">
-                    <label htmlFor="username" className="form-label">Username</label>
-                    <input type="text" className="username form-control"/>
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                <div className="col">
+                  <div className="my-2">
+                      <label htmlFor="username" className="form-label">Username</label>
+                      <input type="text" className="username form-control"/>
+                      <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                  </div>
                 </div>
-
+                
                 <div className="col">
                     <div className="mb-2">
                       <label htmlFor="password" className="form-label">Password </label>
                       <input type="password" className="password form-control"/>    
                     </div>
                 </div>
-
-              </div>
-          </div>
-          </div> {/* end container-lg*/}
+              </div> {/* end row */}
+          </form> {/* end container-lg*/}
       </div>
     </>
   )
