56b847be03bb5cdbc2cf99240a4ba4764fcb3ee5
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-12 08:16:55 +0800

    Subject: Body

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,7 +13,20 @@ function App() {
           </div>
 
           <div className="row">
+              <div className="col">
+                <div className="my-2">
+                    <label htmlFor="username" className="form-label">Username</label>
+                    <input type="text" className="username form-control"/>
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
 
+                <div className="col">
+                    <div className="mb-2">
+                      <label htmlFor="password" className="form-label">Password </label>
+                      <input type="password" className="password form-control"/>    
+                    </div>
+                </div>
+              </div>
           </div>
       </div>
     </>
