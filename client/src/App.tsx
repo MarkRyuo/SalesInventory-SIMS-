0f16d3a378b632325f7061fab25aaa0e9ac8953f
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-13 18:20:10 +0800

    Subject: Body

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -33,7 +33,7 @@ function App() {
                   <div className="row">
                     <div className="col-12">
                       <div className="my-2">
-                          <label htmlFor="username" className="form-label"><IoPersonOutline />Username</label>
+                          <label htmlFor="username" className="form-label"><IoPersonOutline/>Username</label>
                           <input type="text" className="username form-control"/>
                           <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                       </div>
